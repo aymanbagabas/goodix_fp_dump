6a070e66715dacaf48e11de4aa31e20af980e267
Antonio Ospite <antonio.ospite@collabora.com>
2019-10-09 19:36:41 +0200

    extract_images_from_pcap.sh: more robust parsing of tshark output
    tshark may or may not print colons in the output, probably depending on
    the tool version, handle tshark output in a more robust and portable
    way.

--- experiments/extract_images_from_pcap.sh
+++ experiments/extract_images_from_pcap.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
@@ -10,7 +10,10 @@ tshark -r "$1" -Y 'frame.len > 1000' -T fields -e usb.capdata | \
 while read -r line
 do
   suffix=$(printf "%04d" $i)
-  echo -n -e "\\x${line//:/\\x}" > image_packet_$suffix.bin
+  hex_string=$(printf "%s" "$line" | sed -e 's/://g' -e 's/../\\x&/g')
+
+  # shellcheck disable=SC2059
+  printf "$hex_string" > "image_packet_$suffix.bin"
 
   ./decode_image_packet.py "image_packet_$suffix.bin" "$suffix"
   ./convert_to_pseudo_colors.py "unpacked_image_$suffix.bin" "$suffix"
